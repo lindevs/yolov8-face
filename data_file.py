99c0ae8a5e0b2a3ecb0f66d570c9012db8f7f49c
lindevs <lindevscom@gmail.com>
2023-12-17 14:47:19 +0200

    script for data file

---
+++ data_file.py
@@ -0,0 +1,14 @@
+import argparse
+import os
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--example', type=str, default='data/widerface.yaml.example')
+    opt = parser.parse_args()
+
+    filename = os.path.splitext(os.path.relpath(opt.example))[0]
+
+    with open(opt.example, 'r') as fin:
+        with open(filename, 'w') as fout:
+            fout.write(fin.read().replace('PROJECT_DIR', os.getcwd()))
